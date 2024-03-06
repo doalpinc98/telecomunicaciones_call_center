b6f83236bbdcec6fa2f14b6c87752e243751b30d
Doménica Pincay <domenicapincay_98@outlook.com>
2024-03-05 23:00:24 -0500

    archivos de jupyter

--- reports/extraer_bd.py
+++ reports/extraer_bd.py
@@ -1,17 +1,13 @@
-# importar librerías
 import pandas as pd
-from sqlalchemy import create_engine
-import os, sys
-import psycopg2
-
-
-
-
-db_config = {'user': os.getenv("user_bd"),         # nombre de usuario
-             'pwd': os.getenv("pass_bd"), # contraseña
-             'host': 'rc1b-wcoijxj3yxfsf3fs.mdb.yandexcloud.net',
+from sqlalchemy import create_engine, inspect
+import os
+from dotenv import load_dotenv
+load_dotenv()
+db_config = {'user':os.getenv('tripleten_sql_user'),         # nombre de usuario
+             'pwd': os.getenv('tripleten_sql_pwd'), # contraseña
+             'host': os.getenv('tripleten_sql_host'),
              'port': 6432,              # puerto de conexión
-             'db': 'data-analyst-final-project-db'}          # nombre de la base de datos
+             'db': os.getenv('tripleten_sql_db')}          # nombre de la base de datos
 
 connection_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_config['user'],
                                                                      db_config['pwd'],
@@ -19,6 +15,14 @@ connection_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_config['user'],
                                                                        db_config['port'],
                                                                        db_config['db'])
 
-engine = create_engine(connection_string, connect_args={'sslmode':'require'})
 
+engine = create_engine(connection_string, connect_args={'sslmode':'require'},pool_size=10, max_overflow=20)
+inspector=inspect(engine)
+schemas=inspector.get_schema_names()
 
+for schema in schemas:
+    print("schema: %s" % schema)
+    for table_name in inspector.get_table_names(schema=schema):
+        print(f'table_name: ',table_name, f'----------------------------')
+        for column in inspector.get_columns(table_name,schema=schema):
+            print("Column: %s" % column)
